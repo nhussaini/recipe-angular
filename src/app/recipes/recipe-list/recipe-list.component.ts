e93d3e2687ecb07c70367b131bed62742ff3876c
nhussaini <nasrullah_hussaini@yahoo.com>
2022-02-05 15:05:25 -0500

    manage recipes in Recipe service

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
@@ -9,22 +10,28 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
   @Output() recipeWasSelected = new EventEmitter<Recipe>();
-  recipes: Recipe[] = [
-    new Recipe(
-      'A test Recipe',
-      'This is simply a test',
-      'https://i2.wp.com/mamaloli.com/wp-content/uploads/2011/08/bbqchicken-7.jpg?resize=960%2C640'
-    ),
-    new Recipe(
-      'Another test Recipe',
-      'This is simply a test',
-      'https://i2.wp.com/mamaloli.com/wp-content/uploads/2011/08/bbqchicken-7.jpg?resize=960%2C640'
-    ),
-  ];
 
-  constructor() {}
+  //this code was used for using service
+  // recipes: Recipe[] = [
+  //   new Recipe(
+  //     'A test Recipe',
+  //     'This is simply a test',
+  //     'https://i2.wp.com/mamaloli.com/wp-content/uploads/2011/08/bbqchicken-7.jpg?resize=960%2C640'
+  //   ),
+  //   new Recipe(
+  //     'Another test Recipe',
+  //     'This is simply a test',
+  //     'https://i2.wp.com/mamaloli.com/wp-content/uploads/2011/08/bbqchicken-7.jpg?resize=960%2C640'
+  //   ),
+  // ];
 
-  ngOnInit(): void {}
+  recipes: Recipe[] = [];
+
+  constructor(private recipeService: RecipeService) {}
+
+  ngOnInit(): void {
+    this.recipes = this.recipeService.getRecipes();
+  }
 
   onRecipeSelected(recipe: Recipe) {
     this.recipeWasSelected.emit(recipe);
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,1 +1,20 @@
-export class RecipeService {}
+import { Recipe } from './recipe.model';
+
+export class RecipeService {
+  private recipes: Recipe[] = [
+    new Recipe(
+      'A test Recipe',
+      'This is simply a test',
+      'https://i2.wp.com/mamaloli.com/wp-content/uploads/2011/08/bbqchicken-7.jpg?resize=960%2C640'
+    ),
+    new Recipe(
+      'Another test Recipe',
+      'This is simply a test',
+      'https://i2.wp.com/mamaloli.com/wp-content/uploads/2011/08/bbqchicken-7.jpg?resize=960%2C640'
+    ),
+  ];
+
+  getRecipes() {
+    return this.recipes.slice();
+  }
+}
