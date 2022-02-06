35d00671cb5e91c6993c60c48582bf7f186d4df7
nhussaini <nasrullah_hussaini@yahoo.com>
2022-02-05 22:08:18 -0500

    add ingredients to recipes

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,4 +1,5 @@
 import { EventEmitter } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 import { Recipe } from './recipe.model';
 
@@ -7,14 +8,16 @@ export class RecipeService {
 
   private recipes: Recipe[] = [
     new Recipe(
-      'A test Recipe',
-      'This is simply a test',
-      'https://i2.wp.com/mamaloli.com/wp-content/uploads/2011/08/bbqchicken-7.jpg?resize=960%2C640'
+      'Tasty Schnitzel',
+      'A super tasty Schnitzel - just awsome!',
+      'https://www.thespruceeats.com/thmb/LeyN-7W9T0KB2nl6pcuDZckHnjc=/4288x2848/filters:fill(auto,1)/wiener-schnitzel-recipe-1447089-Hero-5b587d6c46e0fb0071b0059d.jpg',
+      [new Ingredient('Meat', 1), new Ingredient('French Fries', 20)]
     ),
     new Recipe(
-      'Another test Recipe',
-      'This is simply a test',
-      'https://i2.wp.com/mamaloli.com/wp-content/uploads/2011/08/bbqchicken-7.jpg?resize=960%2C640'
+      'Big Fat Burger',
+      'What else you need to say?',
+      'https://menu-images-static.skipthedishes.com/images/resized/item-1139b73f91f9e2fb9587.jpg',
+      [new Ingredient('Buns', 2), new Ingredient('Meat', 1)]
     ),
   ];
 
