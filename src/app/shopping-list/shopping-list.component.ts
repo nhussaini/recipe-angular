aa9201982f51918e1fedda855157c287e652b7cf
nhussaini <nasrullah_hussaini@yahoo.com>
2022-02-09 12:47:43 -0500

    improve the reactive service with Subject

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from './shopping-list.service';
 @Component({
@@ -6,24 +7,26 @@ import { ShoppingListService } from './shopping-list.service';
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css'],
 })
-export class ShoppingListComponent implements OnInit {
+export class ShoppingListComponent implements OnInit, OnDestroy {
   // ingredients: Ingredient[] = [
   //   new Ingredient('Apples', 5),
   //   new Ingredient('Tomatoes', 10),
   // ];
 
   ingredients: Ingredient[] = [];
+  private igChangeSub: Subscription;
 
   constructor(private slService: ShoppingListService) {}
 
-  ngOnInit(): void {
+  ngOnInit() {
     this.ingredients = this.slService.getIngredients();
-    this.slService.ingredientsChanged.subscribe((ingredients: Ingredient[]) => {
-      this.ingredients = ingredients;
-    });
+    this.igChangeSub = this.slService.ingredientsChanged.subscribe(
+      (ingredients: Ingredient[]) => {
+        this.ingredients = ingredients;
+      }
+    );
   }
-
-  // onIngredientAdded(ingredient: Ingredient) {
-  //   this.ingredients.push(ingredient);
-  // }
+  ngOnDestroy(): void {
+    this.igChangeSub.unsubscribe();
+  }
 }
