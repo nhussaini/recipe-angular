2e7c70d0c2da2aa59fc8b379d90267bb029f1d1d
nhussaini <nasrullah_hussaini@yahoo.com>
2022-02-28 21:33:31 -0500

    add an auth feature module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,13 +11,13 @@ import { HeaderComponent } from './header/header.component';
 // import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 // import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 // import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+// import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 // import { DropdownDirective } from './shared/dropdown.directive';
 // import { ShoppingListService } from './shopping-list/shopping-list.service';
 // import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
 // import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 // import { RecipeService } from './recipes/recipe.service';
-import { AuthComponent } from './auth/auth.component';
+// import { AuthComponent } from './auth/auth.component';
 // import { LoadingSpinnerComponent } from './shared/loading-spinner/loading-spinner.component';
 // import { AuthInterceptorService } from './auth/auth-interceptor.service';
 // import { AlertComponent } from './shared/alert/alert.component';
@@ -26,17 +26,17 @@ import { RecipesModule } from './recipes/recipes.module';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core.module';
+import { AuthModule } from './auth/auth.module';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, AuthComponent],
+  declarations: [AppComponent, HeaderComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule,
-    ReactiveFormsModule,
     HttpClientModule,
     RecipesModule,
     ShoppingListModule,
+    AuthModule,
     SharedModule,
     CoreModule,
   ],
