fb196a055b1189faa086686c1cf6ba3d6ddf66a6
nhussaini <nasrullah_hussaini@yahoo.com>
2022-02-28 21:19:16 -0500

    add the core module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,18 +13,19 @@ import { HeaderComponent } from './header/header.component';
 // import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 // import { DropdownDirective } from './shared/dropdown.directive';
-import { ShoppingListService } from './shopping-list/shopping-list.service';
+// import { ShoppingListService } from './shopping-list/shopping-list.service';
 // import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
 // import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
-import { RecipeService } from './recipes/recipe.service';
+// import { RecipeService } from './recipes/recipe.service';
 import { AuthComponent } from './auth/auth.component';
 // import { LoadingSpinnerComponent } from './shared/loading-spinner/loading-spinner.component';
-import { AuthInterceptorService } from './auth/auth-interceptor.service';
-import { AlertComponent } from './shared/alert/alert.component';
+// import { AuthInterceptorService } from './auth/auth-interceptor.service';
+// import { AlertComponent } from './shared/alert/alert.component';
 // import { PlaceholderDirective } from './shared/placeholder/placeholder.directive';
 import { RecipesModule } from './recipes/recipes.module';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
 import { SharedModule } from './shared/shared.module';
+import { CoreModule } from './core.module';
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent, AuthComponent],
@@ -37,16 +38,9 @@ import { SharedModule } from './shared/shared.module';
     RecipesModule,
     ShoppingListModule,
     SharedModule,
+    CoreModule,
   ],
-  providers: [
-    ShoppingListService,
-    RecipeService,
-    {
-      provide: HTTP_INTERCEPTORS,
-      useClass: AuthInterceptorService,
-      multi: true,
-    },
-  ],
+  // providers: [],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
